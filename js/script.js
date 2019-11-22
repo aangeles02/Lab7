791e3a8887be0157bca4e4c6411e07860734a490
aangeles02 <54568738+aangeles02@users.noreply.github.com>
2019-11-21 22:35:01 -0600

    final

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 function init(){
-var input;
-var header = document.getElementById("textoutput");
-var button = document.getElementById("entrybutton");
-
-button.addEventListener("click",function()
-{
-	
-input = document.getElementById("entryinput").value;	
-alert("Anthony Angeles: " + input);
-header.innerHTML = input;
+	var input;
+	var header = document.getElementById("textoutput");
+	var button = document.getElementById("entrybutton");
+	button.addEventListener("click",function()
+{	
+		input = document.getElementById("entryinput").value;	
+		alert("Anthony Angeles: " + input);
+		header.innerHTML = input;
 });
 }
 window.addEventListener('load', init);
