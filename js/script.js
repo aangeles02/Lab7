1cadf2716bd646e193b5cf38810c780c0f059372
aangeles02 <54568738+aangeles02@users.noreply.github.com>
2019-11-21 22:22:33 -0600

    added header to change text

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 function init(){
 var user_input;
+var header = document.getElementById("textoutput");
 var button = document.getElementById("entrybutton");
+
 button.addEventListener("click",function(){
+	
 user_input = document.getElementById("entryinput").value;	
 alert("Anthony Angeles: " + input);
+header.innerHTML = input;
 });
 }
 
