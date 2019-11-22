a0cec012790114b308e56cb8655a5b49505a4546
aangeles02 <54568738+aangeles02@users.noreply.github.com>
2019-11-21 22:33:32 -0600

    got rid of character

--- js/script.js
+++ js/script.js
@@ -1,20 +1,14 @@
 function init(){
-var user_input;
+var input;
 var header = document.getElementById("textoutput");
 var button = document.getElementById("entrybutton");
 
 button.addEventListener("click",function()
 {
 	
-user_input = document.getElementById("entryinput").value;	
+input = document.getElementById("entryinput").value;	
 alert("Anthony Angeles: " + input);
 header.innerHTML = input;
 });
 }
-
-
-
-
-
-
 window.addEventListener('load', init);
