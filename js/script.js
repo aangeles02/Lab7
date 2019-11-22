1ebcd649d56427ee073f70d8ddf4b63421034a65
aangeles02 <54568738+aangeles02@users.noreply.github.com>
2019-11-21 22:30:39 -0600

    test

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ var user_input;
 var header = document.getElementById("textoutput");
 var button = document.getElementById("entrybutton");
 
-button.addEventListener("click",function(){
+button.addEventListener("click",function()
+{
 	
 user_input = document.getElementById("entryinput").value;	
 alert("Anthony Angeles: " + input);
